c135bdc9fed5e88281564c4d068d2b699259d247
bobr_zlober <bobr.zlober@gmail.com>
2026-02-26 14:33:03 +0200

    upgraded generato

--- Control.cs
+++ Control.cs
@@ -1,52 +1,79 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 public partial class Control : Godot.Control
 {
-	[Export] public LineEdit nickname_inputField;
-	private string[] firstNames = { 
-    "Толік", "Васька", "Петя", "Масяня", "Гріша", 
-    "Мішаня", "Коля", "Саня", "Вітьок", "Андрюха", 
-    "Льоха", "Паша", "Жорик", "Славік", "Валерка", 
-    "Дімон", "Тьомка", "Гєна", "Тоха", "Стасік",
-	"Артур", "Архіпчик", "Деньчик"
-	};
-	private string[] lastNames = { 
-    "Драйвер", "Агроном", "Напівпровідник", "Спічка", "Каніфоль", 
-    "Шифер", "Транзистор", "Процесор", "Паяльник", "Болгарка", 
-    "Роутер", "Закодований", "Соляра", "Піксель", "Фотон", 
-    "Домушник", "Лінуксер", "Задрот", "Фотік", "Тостер",
-	"Водолаз","Кругляк","Історік","Археолог", "Монстр",
-	"Дотер", "Майнкрафтер"
-	};
-	public override void _Ready()
-	{
-    Button rndButton = GetNode<Button>("nicknameRandomize_button"); 
-    rndButton.Pressed += _on_pressed;
-	}
-	public override void _Process(double delta)
-	{
-	}
-	public void _on_pressed()
-	{
-		string n = firstNames[Generator.Get(firstNames.Length)];
-		string s = lastNames[Generator.Get(lastNames.Length)];
-		string fullName = "";
-		foreach(string part in new string[] { n, s })
-		{
-			fullName += part + " ";
-		}
-        nickname_inputField.Text = fullName.Trim();
-	}
-}
-public static class Generator
-{
-    private static ulong _state = (ulong)System.Environment.TickCount;
+    [Export] public LineEdit nickname_inputField;
 
-    public static int Get(int max) 
+    private string[] firstNames = { "Толік","Васька","Петя","Масяня","Гріша","Мішаня","Коля","Саня","Вітьок","Андрюха",
+        "Льоха","Паша","Жорик","Славік","Валерка","Дімон","Тьомка","Гєна","Тоха","Стасік",
+        "Артур","Архіпчик","Деньчик" };
+    private string[] lastNames = { "Драйвер","Агроном","Напівпровідник","Спічка","Каніфоль","Шифер","Транзистор","Процесор",
+        "Паяльник","Болгарка","Роутер","Закодований","Соляра","Піксель","Фотон","Домушник",
+        "Лінуксер","Задрот","Фотік","Тостер","Водолаз","Кругляк","Історік","Археолог",
+        "Монстр","Дотер","Майнкрафтер" };
+
+    private Random rng = new Random();
+
+    private IEnumerator<int> _firstEnumerator;
+    private IEnumerator<int> _lastEnumerator;
+
+    public IEnumerable<int> IndexGenerator(int arrayLength)
     {
-        _state = (214013 * _state + 2531011) % 2147483648;
-        int result = (int)(_state >> 16) % max;
-        return result;
+        while (true)
+        {
+            yield return rng.Next(0, arrayLength);
+        }
+    }
+
+    public void ConsumeWithTimeout(IEnumerable<string> generator, double seconds)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        int count = 0;
+		
+
+        foreach (var nickname in generator)
+        {
+            if (sw.Elapsed.TotalSeconds >= seconds) break;
+            GD.Print($"[{sw.Elapsed.TotalSeconds:F2}s],#{count} {nickname}");
+			count++;
+			System.Threading.Thread.Sleep(200);
+        }
+    }
+
+    public IEnumerable<string> NicknameGenerator()
+    {
+        var first = IndexGenerator(firstNames.Length).GetEnumerator();
+        var last = IndexGenerator(lastNames.Length).GetEnumerator();
+        while (true)
+        {
+            first.MoveNext();
+            last.MoveNext();
+            yield return $"{firstNames[first.Current]} {lastNames[last.Current]}";
+        }
+    }
+
+    public override void _Ready()
+    {
+        _firstEnumerator = IndexGenerator(firstNames.Length).GetEnumerator();
+        _lastEnumerator = IndexGenerator(lastNames.Length).GetEnumerator();
+
+        GetNode<Button>("nicknameRandomize_button").Pressed += _on_pressed;
+        GetNode<Button>("nicknameRandomize_button").Pressed += _on_consume_pressed;
+    }
+
+    public void _on_pressed()
+    {
+        _firstEnumerator.MoveNext();
+        _lastEnumerator.MoveNext();
+        nickname_inputField.Text = $"{firstNames[_firstEnumerator.Current]} {lastNames[_lastEnumerator.Current]}";
+    }
+
+    public void _on_consume_pressed()
+    {
+        Task.Run(() => ConsumeWithTimeout(NicknameGenerator(), 2.0));
     }
 }
