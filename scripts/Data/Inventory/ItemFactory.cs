4dc88573d58807029c063926114f7d6766ea9d6a
bobr_zlober <bobr.zlober@gmail.com>
2026-05-08 00:45:57 +0300

    add inventory data structures and ItemFactory base

---
+++ scripts/Data/Inventory/ItemFactory.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Godot;
+public partial class ItemFactory : Node
+{
+    private List<ItemTemplate> _itemTemplates = new List<ItemTemplate>();
+    private List<TalismanTemplate> _talismanTemplates = new List<TalismanTemplate>();
+    private readonly Random _rng = new Random();
+    public override void _Ready()
+    {
+        if (FileAccess.FileExists("res://data/InventorySystem/ItemTemplates.json"))
+        {
+            try
+            {
+                using var file = FileAccess.Open("res://data/InventorySystem/ItemTemplates.json", FileAccess.ModeFlags.Read);
+                string json = file.GetAsText();
+                _itemTemplates = JsonSerializer.Deserialize<List<ItemTemplate>>(json);
+            } catch(Exception ex)
+            {
+                GD.PrintErr($"Error ({ex}) occured during opening ItemTemplates.json");
+            }
+        }
+        if(FileAccess.FileExists("res://data/InventorySystem/TalismanTemplates.json"))
+        {
+            try
+            {
+                using var file = FileAccess.Open("res://data/InventorySystem/TalismanTemplates.json", FileAccess.ModeFlags.Read);
+                string json = file.GetAsText();
+                _talismanTemplates = JsonSerializer.Deserialize<List<TalismanTemplate>>(json);
+            } catch(Exception ex)
+            {
+                GD.PrintErr($"Error ({ex}) occured during opening TalismanTemplates.json");
+            }
+        }   
+    }
+    
+    public ItemInstance CreateItem(string TemplateID)
+    {
+        var template = _itemTemplates.FirstOrDefault(t => t.ID == TemplateID);
+        if(template != null)
+        {
+            var instance = new ItemInstance();
+            float statSum = 0f;
+            foreach(var stat in template.Stats)
+            {
+                float value = (float)(_rng.NextDouble() * (stat.Value.Max - stat.Value.Min) + stat.Value.Min);
+                instance.ItemStats[stat.Key] = value;
+                statSum += value;
+            }
+            instance.TemplateID = TemplateID;
+            instance.Price = template.PriceBase * ((decimal)statSum / template.Stats.Count);
+            return instance;
+            
+        }
+        else
+        {
+            GD.PrintErr($"Template{TemplateID} not found");
+            return null;
+        }
+    }
+}
+
